22176af1fd9f4057f34d8428a3cad7d3e2edc113
Your Name <you@example.com>
2023-09-21 20:53:35 +0200

    fixed stuff in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import styles from './Header.module.css'
+import React from 'react';
+import { NavButton } from './Button';
+import styles from './Header.module.css';
+import { FaDiscord, FaTwitter, FaGlobeAmericas} from 'react-icons/fa';
 
-export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
+export const Header: React.FC<React.PropsWithChildren<any>> = ({ children }) => {
   return (
     <div className={styles.wrapper}>
       <div>
-        <div style={{ display: 'flex', gap: '10px' }}>
-          <NavLink className={styles.logo} to="/">
-            <img alt="PEPESOLANA" src="/logo-2.png" height="20px" />
-          </NavLink>
+        <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+          <NavButton variant="ghost" className={styles.logo} to="/">
+            <img src="/logo.png" height="20px" alt="Logo" />
+            <span className={styles.title}>PEPESOLANA CASINO</span>
+          </NavButton>
+          <div className={styles.iconContainer}>
+            <a href="https://discord.gg/QZKCPpMw3g" target="_blank" rel="noreferrer" className={styles.icon}>
+              <FaDiscord />
+            </a>
+            <a href="https://twitter.com/pepesolana_pepe" target="_blank" rel="noreferrer" className={styles.icon}>
+              <FaTwitter />
+            </a>
+            <a href="https://pepesolana.com/" target="_blank" rel="noreferrer" className={styles.icon}>
+              <FaGlobeAmericas />
+            </a>
+          </div>
         </div>
         <div style={{ display: 'flex', gap: '10px' }}>
           {children}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
