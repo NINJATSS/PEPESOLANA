07dc14d9166ecb540c9e22da89b143271fa1bd85
Your Name <you@example.com>
2023-09-25 19:10:58 +0200

    centered stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import { Card } from './components/Card'
-import { Header } from './components/Header'
-import { SlideSection } from './components/Section'
-import { GAMES } from './games'
-import { ErrorHandlers } from './ui/ErrorHandlers'
-import { Game } from './ui/Game'
-import { Home } from './ui/Home'
-import { RecentPlays } from './ui/RecentPlays'
-import { UserButton } from './ui/UserButton'
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { Card } from './components/Card';
+import { Header } from './components/Header';
+import { Section } from './components/Section';
+import { GAMES } from './games';
+import { ErrorHandlers } from './ui/ErrorHandlers';
+import { Game } from './ui/Game';
+import { Home } from './ui/Home';
+import { RecentPlays } from './ui/RecentPlays';
+import { UserButton } from './ui/UserButton';
+
+// The new CenteredCardContainer component
+const CenteredCardContainer: React.FC = ({ children }) => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+    }}>
+      {children}
+    </div>
+  );
+}
 
 export function App() {
-  const location = useLocation()
+  const location = useLocation();
 
-  React.useEffect(() =>
+  React.useEffect(() => {
     document.body.scrollTo({
       top: 0,
       left: 0,
-    })
-  , [location.key])
+    });
+  }, [location.key]);
 
   return (
     <>
@@ -33,13 +46,15 @@ export function App() {
         <Route path="/:shortName" element={<Game />} />
       </Routes>
 
-      <SlideSection title="FEATURED GAMES ">
-        {GAMES.map((game) => (
-          <Card key={game.short_name} game={game} />
-        ))}
-      </SlideSection>
+      <Section title="FEATURED GAMES ">
+        <CenteredCardContainer>
+          {GAMES.map((game) => (
+            <Card key={game.short_name} game={game} />
+          ))}
+        </CenteredCardContainer>
+      </Section>
 
       <RecentPlays />
     </>
-  )
+  );
 }
