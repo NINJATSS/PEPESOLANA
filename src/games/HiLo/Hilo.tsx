38b80f99da1d039ecf8a37baebb08cefcb2113eb
NINJATSS <143434164+NINJATSS@users.noreply.github.com>
2023-11-29 16:46:22 +0000

    Update Hilo.tsx

--- src/games/HiLo/Hilo.tsx
+++ src/games/HiLo/Hilo.tsx
@@ -104,7 +104,7 @@ export default function HiLo() {
       // }, 500)
       // return
       sounds.play.play()
-      const res = await gamba.play({ bet, wager: profit || initialWager, creatorFee: 0.02 })
+      const res = await gamba.play({ bet, wager: profit || initialWager, creatorFee: 0.03 })
 
       setLoading(true)
       const result = await res.result()
