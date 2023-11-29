2d49682140c1d9927cd5242a64b843e380eb62b4
NINJATSS <143434164+NINJATSS@users.noreply.github.com>
2023-11-29 16:44:29 +0000

    Update App.tsx

--- src/games/Dice/App.tsx
+++ src/games/Dice/App.tsx
@@ -42,7 +42,7 @@ export default function Dice() {
 
       const res = await gamba.play({
         bet,
-        wager, creatorFee: 0.02
+        wager, creatorFee: 0.03
       })
 
       const result = await res.result()
